eed56cc4cf61f5e841f45c1c2ff4436fae01280d
tcdude <tc@tizilogic.com>
2020-06-03 20:05:49 +0200

    Initial AABB with property

--- Sources/scenegraph/AABB.hx
+++ Sources/scenegraph/AABB.hx
@@ -1,57 +1,51 @@
 package scenegraph;
 
 import kha.math.FastVector2;
-import kha.math.Vector2;
+import kha.FastFloat;
 
 
 class AABB {
-    public var pos(get_pos, set_pos):FastVector2;
+    @:isVar public var pos(get, set):FastVector2;
     public var hw:FastFloat;
     public var hh:FastFloat;
 
     public function new(?vpos:FastVector2, ?x:FastFloat, ?y:FastFloat,
                         hw:FastFloat, hh:FastFloat) {
-        set_pos(vpos, x, y);
+        if (vpos != null)
+            this.pos = vpos;
+        else
+            this.pos = new FastVector2(x, y);
         this.hw = hw;
         this.hh = hh;
     }
 
     public function intersect(other: AABB): Bool {
-        var tl = x - hw;
-        var tr = x + hw;
-        var tt = y - hh;
-        var tb = y + hh;
-        var ol = other.x - hw;
-        var or = other.x + hw;
-        var ot = other.y - hh;
-        var ob = other.y + hh;
+        var tl = pos.x - hw;
+        var tr = pos.x + hw;
+        var tt = pos.y - hh;
+        var tb = pos.y + hh;
+        var ol = other.get_pos().x - hw;
+        var or = other.get_pos().x + hw;
+        var ot = other.get_pos().y - hh;
+        var ob = other.get_pos().y + hh;
         return ((((ol > tl && ol < tr) || (or > tl && or < tr))
                  && ((ot > tt && ot < tb) || (ob > tt && ob < tb))) // Partial intersect
                 || (ol < tl && or > tr && ot < tt && ob > tb)); // This inside other
     }
 
-    public function vec_inside(other: Vector2): Bool {
-        var tl = x - hw;
-        var tr = x + hw;
-        var tt = y - hh;
-        var tb = y + hh;
+    public function vec_inside(other: FastVector2): Bool {
+        var tl = pos.x - hw;
+        var tr = pos.x + hw;
+        var tt = pos.y - hh;
+        var tb = pos.y + hh;
         return (other.x > tl && other.x < tr && other.y > tt && other.y < tb);
     }
 
-    public function get_pos():FastVector2 {
+    public function get_pos() {
         return pos;
     }
 
-    public function set_pos(?vpos:FastVector2, ?x:FastFloat, ?y:FastFloat) {
-        if (vpos != null) {
-            pos = vpos;
-        }
-        else if (x != null && y != null) {
-            pos.x = x;
-            pos.y = y;
-        }
-        else {
-            throw "Either pass a vector or both components of it";
-        }
+    public function set_pos(pos:FastVector2) {
+        return this.pos = pos;
     }
 }
